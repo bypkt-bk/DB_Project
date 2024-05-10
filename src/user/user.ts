55cf8b2ebf9a6042dde3d0063e5fe567e6e02ac8
banyaphon <92526947+bypkt-bk@users.noreply.github.com>
2024-05-10 09:21:02 +0700

    final

--- src/user/user.ts
+++ src/user/user.ts
@@ -32,6 +32,42 @@ user.post("/", async (c) => {
 
 
 
+// user.get("/:param", async (c) => {
+//   const param = c.req.param("param");
+
+//   if (!isNaN(Number(param))) {
+//       // If the parameter is a number, assume it's an ID
+//       const result = await prisma.user.findUnique({
+//           where: {
+//               user_id: Number.parseInt(param),
+//           },
+//       });
+//       if (result) {
+//           return c.json({ result });
+//       } else {
+//           return c.json({ result: "not found" }, 404);
+//       }
+//   } else {
+//       // If the parameter is not a number, assume it's a name
+//       const result = await prisma.user.findMany({
+//           where: {
+//               OR: [
+//                   {
+//                       first_name: { contains: param }
+//                   },
+//                   {
+//                       last_name: { contains: param }
+//                   }
+//               ]
+//           }
+//       });
+//       if (result.length !== 0) {
+//           return c.json({ result });
+//       } else {
+//           return c.json({ result: "not found" }, 404);
+//       }
+//   }
+// });
 
 
 export default user;
